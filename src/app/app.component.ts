998cfa0032ff0aeafc9381951dc10764e074d068
lockheeed <anonimus140119@gmail.com>
2020-10-27 16:02:04 +0300

    test2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test2';
+
+  firstOperand: number;
+  secondOperand: number;
+
+  invertNeeded: string = "none";
+  operation: string;
+
+  result: boolean;
+  error: string;
+
+  tempResult: number;
+
+  submitFunc() {
+    this.result = undefined;
+    this.error = "NoN";
+
+    if (this.firstOperand == undefined || this.firstOperand == undefined || this.operation == undefined) {
+      this.error = "Необходимо заполнить все поля!";
+    } else {
+      if (this.operation == "||") { this.tempResult = this.firstOperand || this.secondOperand; }
+      else if (this.operation == "&&") { this.tempResult = this.firstOperand && this.secondOperand; }
+
+      console.log(this.tempResult);
+
+      if (this.invertNeeded == "true") {
+        console.log("needed");
+        if (this.tempResult == 0) { this.result = true; }
+        else { this.result = false; }
+      } else {
+        console.log("not needed");
+        if (this.tempResult == 0) { this.result = false; }
+        else { this.result = true; }
+      }
+
+      console.log(this.result);
+    }
+  }
 }
